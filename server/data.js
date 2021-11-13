fe2c1014d32ef2460dfb659e4b6966a1cdb8ba61
Adam Iverson <aciverso@gmail.com>
2021-11-12 21:44:06 -0600

    server and data files setup

--- server/data.js
+++ server/data.js
@@ -1,1 +1,4 @@
+// which data goes here?
+// it's probably an array of objects
+
 module.exports = 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+// const blahblahblah = require('whatever data.js exports?')
+
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
+app.use(
 
 app.get('/', (req, res) => {
     console.log('in GET /');
